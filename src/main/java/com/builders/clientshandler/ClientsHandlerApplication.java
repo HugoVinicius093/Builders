f49b7ee0edacc913abc6fcdf19af94d5ef828b9e
hugovini093 <hugo.vinicius093@gmail.com>
2021-07-26 00:12:32 -0300

    Melhorias no properties da aplicação e configuração docker-compose

--- src/main/java/com/builders/clientshandler/ClientsHandlerApplication.java
+++ src/main/java/com/builders/clientshandler/ClientsHandlerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ClientsHandlerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientsHandlerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClientsHandlerApplication.class, args);
+    }
 
 }
