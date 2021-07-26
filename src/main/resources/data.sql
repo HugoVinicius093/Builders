9406972c07e660b16452a68cb270e6951c10d23f
hugovini093 <hugo.vinicius093@gmail.com>
2021-07-26 19:17:51 -0300

    Inserção de dados na inicialização

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO public.clients
-(cep, cidade, cpf, data_nascimento, endereco, estado, nome, sexo)
-VALUES('19040-111', 'Campinas', '456359874567', '20210505', 'Rua das rosas', 'SP', 'Cristiano Reinaldo', 'M');
-INSERT INTO public.clients
-(cep, cidade, cpf, data_nascimento, endereco, estado, nome, sexo)
-VALUES('19050-222', 'Jundiai', '456359878567', '20210506', 'Rua Augusta', 'SP', 'Alexandro Ganso', 'F');
-INSERT INTO public.clients
-(cep, cidade, cpf, data_nascimento, endereco, estado, nome, sexo)
-VALUES('19060-333', 'Vinhedo', '456359874568', '20210507', 'Rua Brasil Novo', 'SP', 'Paulo Henrique Pato', 'M');
+delete FROM clients c;
+
+INSERT INTO clients (cep,cidade,cpf,data_nascimento,email,endereco,estado,nome) VALUES
+('19060-555','Cidade A','76203621005','1998-12-20','KMbappe@gmail.com','Rua xyz','SP','Kylian Mbappe')
+,('19040-185','Cidade B','27167518034','1987-12-19','KarimBenzema@gmail.com','Rua abc','MG','Karim Benzema')
+,('19060-555','Cidade C','88399806013','1991-03-21','AntonieGriezmann@gmail.com','Rua xpa','BH','Antoine Griezmann')
+,('19060-643','Cidade D','95897794090','1993-03-15','PaulPogba@gmail.com','Rua kxs','RJ','Paul Pogba')
+,('19060-643','Cidade E','73307749013','1986-09-30','GiroudO@gmail.com','Rua kxs','RJ','Olivier Giroud')
+;
