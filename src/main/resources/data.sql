e4515034dbc013251c9c67a9b65f2b54ba21785d
hugovini093 <hugo.vinicius093@gmail.com>
2021-07-26 20:10:19 -0300

    Criação do postman collection e ajustes nos properties.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,19 @@
-delete FROM clients c;
+DROP TABLE IF EXISTS public.clients;
+
+CREATE TABLE IF NOT exists public.clients (
+	id bigserial NOT NULL,
+	cep varchar(15) NOT NULL,
+	cidade varchar(50) NOT NULL,
+	cpf varchar(14) NOT NULL,
+	data_nascimento date NULL,
+	email varchar(100) NOT NULL,
+	endereco varchar(100) NOT NULL,
+	estado varchar(25) NOT NULL,
+	nome varchar(100) NOT NULL,
+	CONSTRAINT clients_pkey PRIMARY KEY (id),
+	CONSTRAINT uk7it9dgecuhaofss241235vdcn UNIQUE (cpf),
+	CONSTRAINT uksrv16ica2c1csub334bxjjb59 UNIQUE (email)
+);
 
 INSERT INTO clients (cep,cidade,cpf,data_nascimento,email,endereco,estado,nome) VALUES
 ('19060-555','Cidade A','76203621005','1998-12-20','KMbappe@gmail.com','Rua xyz','SP','Kylian Mbappe')
